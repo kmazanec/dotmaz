fac98aec2def272a5de89574e238ca4f2765b536
Keith Mazanec <k.mazanec@gmail.com>
2015-03-20 10:48:10 -0500

    setup script does key repeats speeds

--- setup/setup.sh
+++ setup/setup.sh
@@ -10,3 +10,8 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 echo 'Setting up .files'
 
 ln -s $PWD/.bash_profile ~/.bash_profile
+
+echo 'Setting up keyboard preferences'
+
+defaults write -g InitialKeyRepeat -int 14
+defaults write -g KeyRepeat -int 1
