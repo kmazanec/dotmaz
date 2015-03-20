321fa0d80e6ead2d33ed8ce7fb67404c74a7e0c6
Keith Mazanec <k.mazanec@gmail.com>
2015-03-20 15:12:12 -0500

    more complete setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,12 +6,44 @@ echo 'Installing Homebrew...'
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo 'Installing fav brews...'
+
+brew install macvim
+brew install python
+
 
 echo 'Setting up .files'
 
 ln -s $PWD/.bash_profile ~/.bash_profile
+mv ~/.gitconfig ~/.gitconfig.local
+ln -s $PWD/.gitconfig ~/.gitconfig
+
+ln -s $PWD/.vimbundle.local ~/.vimbundle.local
+ln -s $PWD/.vimrc.local ~/.vimrc.local
+ln -s $PWD/.psqlrc ~/.psqlrc
+
+
+echo 'Loading dotmatrix'
+
+pushd ..
+git clone https://github.com/hashrocket/dotmatrix.git
+cd dotmatrix
+
+echo 'Running install script'
+bin/install
+
+echo 'Installing vimbundles'
+bin/vimbundles.sh
+popd
+
+
+echo 'Setting up pathogen'
+
+mkdir ~/.vim/autoload
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 
 echo 'Setting up keyboard preferences'
 
-defaults write -g InitialKeyRepeat -int 14
+defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 1
