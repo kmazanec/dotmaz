3de39601657a179e0b7eb5221d067cd791f0eea7
Keith Mazanec <k.mazanec@gmail.com>
2019-08-06 12:34:00 -0500

    WIP updating setup script for dotfiles

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,61 +1,65 @@
 #! /bin/bash
 
-echo 'Setting up yo machine!'
+# Run this from the dotmaz project root
 
-echo 'Installing Homebrew...'
+echo "Setting up yo machine!"
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Setting up key repeat preferences"
 
-echo 'Installing fav brews...'
-
-brew install macvim
-brew install python
-brew install chruby
-brew install ruby-install
-brew install mercurial
-brew install go
-brew install leiningen
-brew install redis
-
-
-echo 'Setting up .files'
-
-ln -s $PWD/.bash_profile ~/.bash_profile
-mv ~/.gitconfig ~/.gitconfig.local
-ln -s $PWD/.gitconfig ~/.gitconfig
-
-ln -s $PWD/.vimbundle.local ~/.vimbundle.local
-ln -s $PWD/.vimrc.local ~/.vimrc.local
-ln -s $PWD/.psqlrc ~/.psqlrc
-ln -s $PWD/profiles.clj ~/.lein/profiles.clj
-
-
-echo 'Loading dotmatrix'
-
-pushd ..
-git clone https://github.com/hashrocket/dotmatrix.git
-cd dotmatrix
-
-echo 'Running install script'
-bin/install
-
-echo 'Installing vimbundles'
-bin/vimbundles.sh
-popd
-
-
-echo 'Setting up pathogen'
-
-mkdir ~/.vim/autoload
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-
-echo 'Setting up keyboard preferences'
-
-defaults write -g InitialKeyRepeat -int 15
+defaults write -g InitialKeyRepeat -int 12
 defaults write -g KeyRepeat -int 1
 
+if [[ -x "$(command -v brew)" ]]; then
+  echo "Homebrew already installed, updating"
+#  brew update
+else
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-echo "You'll want to install sublime text next: http://www.sublimetext.com/3"
-echo "and then the package manager: https://packagecontrol.io/installation"
-echo "and then run the sublime install script to symlink the files"
+echo "Installing fav brews..."
+
+#brew install macvim
+# brew install python
+#brew install chruby
+#brew install ruby-install
+# brew install mercurial
+#brew install go
+#brew install redis
+
+echo "Setting up .files"
+
+for f in $PWD/dotfiles/.*
+do
+  if [[ -f $f ]]; then
+    fname=$(basename $f)
+    echo "Linking $fname"
+
+    if [[ -f ~/$fname && ! -L ~/$fname ]]; then
+      mv ~/$fname ~/$fname.local
+    else
+      ln -s $f ~/$fname
+    fi
+  fi
+done
+
+# ln -s $PWD/.vimbundle.local ~/.vimbundle.local
+
+# echo 'Loading dotmatrix'
+# 
+# pushd ..
+# git clone https://github.com/hashrocket/dotmatrix.git
+# cd dotmatrix
+# 
+# echo 'Running install script'
+# bin/install
+# 
+# echo 'Installing vimbundles'
+# bin/vimbundles.sh
+# popd
+# 
+# 
+# echo 'Setting up pathogen'
+# 
+# mkdir ~/.vim/autoload
+# curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
